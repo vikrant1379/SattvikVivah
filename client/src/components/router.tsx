d6dd28ca1447cb0251846992dd061d41ec22cacb
Vikrant Chaudhary <cvikrant462@gmail.com>
2025-08-09 23:05:22 +0000

    Restored to 'e84123cd151c65040aebe8c879b2424ba5f30512'
    Replit-Restored-To: e84123cd151c65040aebe8c879b2424ba5f30512

--- client/src/components/router.tsx
+++ client/src/components/router.tsx
@@ -1,15 +1,33 @@
-
-import { Routes, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "wouter";
+import { useEffect } from "react";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 import ProfileBrowser from "@/components/profile-browser";
+import ProfileDetailPage from "@/components/profile-detail-page";
 
-export default function AppRouter() {
+const AppRouter = () => {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    console.log("=== Router Debug ===");
+    console.log("Current location:", location);
+    console.log("Window pathname:", window.location.pathname);
+    console.log("Location type:", typeof location);
+    console.log("Is profile route?", location.startsWith('/profile/'));
+    if (location.startsWith('/profile/')) {
+      const profileId = location.split('/')[2];
+      console.log("Extracted profile ID from router:", profileId);
+    }
+  }, [location]);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/browse" element={<ProfileBrowser />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/browse" component={ProfileBrowser} />
+      <Route path="/profile/:profileId" component={ProfileDetailPage} />
+      <Route component={NotFound} />
+    </Switch>
   );
-}
+};
+
+export default AppRouter;
