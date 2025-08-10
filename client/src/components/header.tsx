98d79de2d8bb8779463245151e0c43f12a6b6993
Vikrant Chaudhary <cvikrant462@gmail.com>
2025-08-10 23:49:51 +0000

    Restored to 'cd265c3906e73c03b7ffca66055e719b6f382cea'
    Replit-Restored-To: cd265c3906e73c03b7ffca66055e719b6f382cea

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -223,6 +223,7 @@ const Header = memo(() => {
                   </DropdownMenuContent>
                 </DropdownMenu>
               </>
+            )
             ) : (
               <div className="flex items-center space-x-3">
                 <Button
